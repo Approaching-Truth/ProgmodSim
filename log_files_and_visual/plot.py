e441e2aa1f114457773b77a890c9b5e4e24d107c
BusterSalomon <54437784+BusterSalomon@users.noreply.github.com>
2023-03-30 14:38:01 +0200

    Some updates
    Co-authored-by: Approaching-Truth <Approaching-Truth@users.noreply.github.com>

--- log_files_and_visual/plot.py
+++ log_files_and_visual/plot.py
@@ -1,14 +1,12 @@
 import sys
 import numpy as np
-import matplotlib.pylab as plt
+import matplotlib
+import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+
 
 # Change default path 
 # sys.path.append('PROGMOD_')
-
-# Get data
-f = open("log_files/sce5_log", "r")
-lines : str = f.read()
 
 # Function for formatting file to array of specific variables
 # data = txt file
@@ -29,43 +27,71 @@ def txt_to_variable_array (data, sh_index, var_index):
 
     # Convert from strings to float
     in_numbers = [float(numerical_string) for numerical_string in seperated]
-    return in_numbers
 
-# GET XY DATA
-x = txt_to_variable_array(lines, 0, 0)
-y = txt_to_variable_array(lines, 0, 1)
+    return (in_numbers, seperated, vars)
+        
 
-# PLOT REFFERNCE LINE
-ref = 3.14/8
-x_start = 0
-x_end = max(x)
-res = 100 # resolution
-x_target = np.linspace(x_start, x_end, res)
-y_target = np.full(100, ref)
-plt.plot(x_target, y_target, color="black", label="Target")
+# def plotxy (x, y, xlab, ylab, xunit, yunit):
+#     # PLOT REFFERNCE LINE
+#     ref = 3.14/8
+#     x_start = 0
+#     x_end = max(x)
+#     res = 100 # resolution
+#     x_target = np.linspace(x_start, x_end, res)
+#     y_target = np.full(100, ref)
+#     plt.plot(x_target, y_target, color="black", label="Target")
 
-# Safe ranges
-N1 = 0
-N2 = 3.1415/4
-x_start = 0
-x_end = max(x)
-res = 100 # resolution
-y_N1 = np.full(res, N1)
-y_N2 = np.full(res, N2)
-plt.plot(x_target, y_N1, color="red", label="N1")
-plt.plot(x_target, y_N2, color="red", label="N2")
+#     # Safe ranges
+#     N1 = 0
+#     N2 = 3.1415/4
+#     x_start = 0
+#     x_end = max(x)
+#     res = 100 # resolution
+#     y_N1 = np.full(res, N1)
+#     y_N2 = np.full(res, N2)
+#     plt.plot(x_target, y_N1, color="red", label="N1")
+#     plt.plot(x_target, y_N2, color="red", label="N2")
+
+#     # PLOT XY DATA
+#     plt.plot(x, y, "ro-", color="blue")
+#     plt.xlabel(xlab)
+#     plt.ylabel(ylab)
+
+#     # SET UNITS
+#     plt.gca().xaxis.set_major_formatter(ticker.FormatStrFormatter(f'%.1f {xunit}'))
+#     plt.gca().yaxis.set_major_formatter(ticker.FormatStrFormatter(f'%.1f {yunit}'))
+
+#     plt.grid()
+#     plt.legend()
+#     plt.show()
+
+
+# Main
+def main():
+    # Changing default matplotlib font: https://jonathansoma.com/lede/data-studio/matplotlib/changing-fonts-in-matplotlib/
+    matplotlib.rcParams['font.serif'] = "Palatino Linotype" # Change default serif font
+    matplotlib.rcParams['font.family'] = "serif" # Set default family to serif
+
+    # Get data
+    f = open("log_files/sce5_log", "r")
+    lines : str = f.read()
+
+    # GET XY DATA
+    x_num, x_str, x_str_seperated = txt_to_variable_array(lines, 0, 0)
+    y_num, y_str, y_str_seperated = txt_to_variable_array(lines, 0, 1)
+    v_num, v_str, v_str_seperated = txt_to_variable_array(lines, 0, 2)
+
+    # Print for table
+    print(x_str_seperated)
+    print(y_str_seperated)
+    print(v_str_seperated)
+
+    #Plt
+    #plotxy(x_num, y_num, "Time", "Angle", "s", "rad")
+    
+    
 
 
 
-# PLOT XY DATA
-plt.plot(x, y, "ro-", color="blue")
-plt.xlabel("x")
-plt.ylabel("y")
-
-# SET UNITS
-# plt.gca().xaxis.set_major_formatter(ticker.FormatStrFormatter('%.1f s'))
-# plt.gca().yaxis.set_major_formatter(ticker.FormatStrFormatter('%.1f rad'))
-
-plt.grid()
-plt.legend()
-plt.show()
+if __name__ == "__main__":
+    main()
